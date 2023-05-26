16284ace59b63e1ddacbb6989eb8cc2d9f5e2c1c
Spencer Schien <spencer.schien@gmail.com>
2023-05-26 13:08:38 -0500

    updated double counting

--- R/fix_bad_ids.R
+++ R/fix_bad_ids.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(wisconsink12)
+
 geo_schools <- read_rds("data/geocoded_wi_schools_2022-23.rda")
 s <- schools |> 
   filter(school_year == "2021-22")
@@ -8,6 +11,12 @@ m <- geo_schools |>
 geo_schools_fixed <- read_csv("../000_data_temp/geocoded_mke_schools.csv")
 
 
+one <- geo_schools_fixed |> 
+  left_join(schools |> filter(school_year == "2020-21") |> 
+              select(dpi_true_id, school_name)) |> 
+  select(dti = dpi_true_id,
+         school_name)
+
 w_fixed <- m |> 
   left_join(geo_schools_fixed |> 
               left_join(schools |> filter(school_year == "2020-21") |> 
@@ -15,11 +24,14 @@ w_fixed <- m |>
               select(dti = dpi_true_id,
                      school_name)) |> 
   filter(!is.na(dti)) |> 
-  mutate(dpi_true_id = dti) |> 
-  select(-dti)
+  mutate(dpi_true_id = dti) 
 
 new <- bind_rows(w_fixed, 
           geo_schools |> 
-            filter(!dpi_true_id %in% w_fixed$dpi_true_id))
+            filter(!dpi_true_id %in% w_fixed$dpi_true_id))  |> 
+  group_by(school_year, district_name, school_name) |> 
+  mutate(n = n() ) |> 
+  ungroup() |> 
+  filter(!(n > 1 & is.na(dti)))
 
 saveRDS(new, "data/geocoded_wi_schools_fixed.rda")
