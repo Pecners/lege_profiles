04ca393c62a98b1428d14ccfb8939f1e49213de3
Spencer Schien <spencer.schien@gmail.com>
2023-07-17 09:35:04 -0500

    catching up

--- R/fix_bad_ids.R
+++ R/fix_bad_ids.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(wisconsink12)
 
-geo_schools <- read_rds("data/geocoded_wi_schools_2022-23.rda")
+geo_schools <- read_rds("../lege_profiles/data/geocoded_wi_schools_2022-23.rda")
+geo2 <- read_rds("../lege_profiles/data/geocoded_wi_schools_fixed.rda")
 s <- schools |> 
   filter(school_year == "2021-22")
 
@@ -24,14 +25,14 @@ w_fixed <- m |>
               select(dti = dpi_true_id,
                      school_name)) |> 
   filter(!is.na(dti)) |> 
-  mutate(dpi_true_id = dti) 
+  mutate(dpi_true_id = dti)
 
 new <- bind_rows(w_fixed, 
           geo_schools |> 
             filter(!dpi_true_id %in% w_fixed$dpi_true_id))  |> 
-  group_by(school_year, district_name, school_name) |> 
-  mutate(n = n() ) |> 
+  group_by(school_year, district_name, school_name, school_type) |> 
+  mutate(n = n()) |> 
   ungroup() |> 
   filter(!(n > 1 & is.na(dti)))
 
-saveRDS(new, "data/geocoded_wi_schools_fixed.rda")
+saveRDS(new, "../000_data_temp/geocoded_wi_schools_fixed.rda")
