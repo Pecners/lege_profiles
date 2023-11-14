f3f5715a030d53d60a8b2e21ebb7ee184c098b69
Spencer Schien <spencer.schien@gmail.com>
2023-11-14 06:46:44 -0600

    added mke dist maps

---
+++ R/mke_dist_maps.R
@@ -0,0 +1,42 @@
+library(rnaturalearth)
+library(glue)
+
+mke <- places("wi") |> 
+  filter(NAME == "Milwaukee")
+
+l <- ne_download(type = "lakes", category = "physical", scale = "small")  %>%
+  st_as_sf(., crs = st_crs(states))
+
+lakes <- c("Lake Michigan")
+
+gl <- l %>%
+  filter(name %in% lakes) %>%
+  st_transform(crs = st_crs(mke)) |> 
+  st_union()
+
+mke_skinny <- st_difference(mke, gl)
+
+mke_skinny |> 
+  ggplot() +
+  geom_sf()
+
+
+leges_sf <- read_rds("data/leges_with_sf_2023.rda")
+
+these <- c("Dora Drake",
+           "Kalan Haywood",
+           "Sylvia Ortiz-Velez")
+
+walk(these, function(t) {
+  this_lege <- leges_sf |> 
+    filter(name == t)
+  
+  mke_skinny |> 
+    ggplot() +
+    geom_sf() +
+    geom_sf(data = this_lege, fill = cfc_darkblue, color = NA) +
+    theme_void()
+  
+  ggsave(glue("maps/{this_lege$name}.png"), bg = "transparent")
+  
+})
